74f4f34c276a9888d0656e54f303dc64550fa4e8
R7vera <josazzsystem@gmail.com>
2025-08-04 20:58:06 -0500

    Levantamiento de errores

--- demo/src/main/java/com/example/ventas/demo/config/OpenApiConfig.java
+++ demo/src/main/java/com/example/ventas/demo/config/OpenApiConfig.java
@@ -1,31 +1,35 @@
 package com.example.ventas.demo.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Sistema de Ventas API",
+        version = "1.0",
+        description = "API REST para sistema de ventas"
+    )
+)
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")))
-                .info(new Info()
-                        .title("API de Ventas")
-                        .description("API REST para sistema de ventas")
-                        .version("1.0"));
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+            .components(new Components()
+                .addSecuritySchemes(securitySchemeName,
+                    new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
     }
 }
